88a32b45f0c702f9e0bbf568716815eed7fa6c34
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 22:09:42 +0900

    feat: implement Enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameContext gameContext;
+    public NavMeshAgent nav;
+    public EnemyData enemyData;
+    public Vector3 lastDest;
+    public bool isDie = false;
+    [field: SerializeField] public PlayerAnimationData animationData { get; private set; }
+    public Animator animator { get; private set; }
+    public CharacterController controller { get; private set; }
+    private EnemyStateMachine stateMachine;
+
+    private void Awake()
+    {
+        animationData = new PlayerAnimationData();
+        animationData.Initialize();
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        stateMachine?.Update();
+    }
+
+    public void initialize(GameContext gameContext, EnemyData enemyData)
+    {
+        this.gameContext = gameContext;
+        this.enemyData = enemyData;
+        stateMachine = new EnemyStateMachine(this);
+        stateMachine.ChangeState(stateMachine.enemyIdleState);
+    }
+
+    public void SetNavAgent()
+    {
+        if (nav == null)
+        {
+            nav = this.AddComponent<NavMeshAgent>();
+
+            int walkableArea = NavMesh.GetAreaFromName("Walkable");
+            if (walkableArea != -1)
+            {
+                nav.areaMask = 1 << walkableArea;
+            }
+        }
+    }
+
+    public void SetDestination()
+    {
+        Vector3 dest = gameContext.player.transform.position;
+        StartCoroutine(WaitForNavMeshAndSetDestination(dest));
+    }
+
+    private IEnumerator WaitForNavMeshAndSetDestination(Vector3 destination)
+    {
+        while (!nav.isOnNavMesh || !nav.isActiveAndEnabled)
+        {
+            yield return null;
+        }
+        nav.SetDestination(destination);
+        lastDest = destination;
+    }
+
+    public void Teleport(Vector3 dest)
+    {
+        transform.position = dest;
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyFactory
+{
+    public Enemy BuildEnemy(GameContext gameContext, EnemyData enemyData)
+    {
+        GameObject enemyRoot = GameObject.Instantiate(enemyData.enemyPrefab);
+        if (enemyData.enemyPrefab != null)
+        {
+            Enemy enemy = enemyRoot.AddComponent<Enemy>();
+            enemy.initialize(gameContext, enemyData);
+            return enemy;
+        }
+        return null;
+    }
+
+    public void DestoryStage(GameObject enemyRoot)
+    {
+        if (enemyRoot != null)
+        {
+            GameObject.Destroy(enemyRoot);
+        }
+    }
+}
