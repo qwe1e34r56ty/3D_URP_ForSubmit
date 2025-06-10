1c56e8f6ad408d8d65ff08b9f46f4bb5b18b9c57
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 21:56:15 +0900

    feat: implement UIManager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIState
+{
+    StageSelect = 0,
+    InGame = 1,
+    Total = 2
+}
+
+public class UIManager : MonoBehaviour
+{
+    private GameContext gameContext;
+    [SerializeField]
+    private IngameUI ingameUI;
+    [SerializeField]
+    private InventoryUI inventoryUI;
+    [SerializeField]
+    private StageSelectUI stageSelectUI;
+
+    private BaseUI[] uiArr = new BaseUI[(int)UIState.Total];
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Initialize(GameContext gameContext)
+    {
+        this.gameContext = gameContext;
+        uiArr[(int)UIState.StageSelect] = stageSelectUI;
+        uiArr[(int)UIState.InGame] = ingameUI;
+        foreach (BaseUI ui in uiArr)
+        {
+            ui.Initialize(gameContext, this);
+        }
+        OpenBaseUI(UIState.StageSelect);
+        inventoryUI.Initialize(gameContext, this);
+    }
+
+    public void OpenBaseUI(UIState state)
+    {
+        foreach (BaseUI ui in uiArr)
+        {
+            ui.SetActive(state);
+        }
+    }
+
+    public void OpenInventoryUI()
+    {
+        inventoryUI.gameObject.SetActive(true);
+    }
+
+    public void CloseInventoryUI()
+    {
+        inventoryUI.gameObject.SetActive(false);
+    }
+}
