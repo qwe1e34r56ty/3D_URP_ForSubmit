4dd3f06c513d9e87b4f6a3c159fa990e7aa17051
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 21:57:23 +0900

    feat: implement StageSelectButton

---
+++ Assets/Scripts/UI/StageSelectButton.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class StageSelectButton : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text buttonText;
+    public void Initialize(StageData stageData)
+    {
+        buttonText?.SetText(stageData.name);
+    }
+}
