ffbc88dfe933713f15947992051388ca34ae4be1
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 22:13:49 +0900

    feat: implement GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameContext gameContext;
+    [SerializeField]
+    private string stageDatasDir;
+    [SerializeField]
+    private string playerDataPath;
+    [SerializeField]
+    public UIManager uiManager;
+
+    private void Awake()
+    {
+        gameContext = new GameContext(stageDatasDir,
+                playerDataPath
+            );
+        uiManager.Initialize(gameContext);
+    }
+
+    public void Update()
+    {
+    }
+
+}
