f141bfeb5bb4ca39b7dc8b8e73156cdc38ae5035
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 21:57:46 +0900

    feat: implement ItemSlot

---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    private GameContext gameContext;
+    [SerializeField]
+    private TMP_Text text;
+    
+    public void Initialize(GameContext gameContext, ItemData itemData)
+    {
+        this.gameContext = gameContext;
+        text?.SetText(itemData.name);
+    }
+}
