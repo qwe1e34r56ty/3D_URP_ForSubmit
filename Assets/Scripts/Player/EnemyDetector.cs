8d69fd1f4c8e5fad5e848b2e843a10ee8c958771
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 22:06:16 +0900

    feat: implement enemyDetector

---
+++ Assets/Scripts/Player/EnemyDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    public HashSet<Enemy> detectedEnemies = new();
+    public float radius = 5f;
+
+    private void Awake()
+    {
+        SphereCollider col = gameObject.AddComponent<SphereCollider>();
+        col.isTrigger = true;
+        col.radius = radius;
+    }
+
+    private void Update()
+    {
+        detectedEnemies.RemoveWhere(enemy => enemy == null);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            detectedEnemies.Add(enemy);
+        }
+    }
+}
