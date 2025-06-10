1d8a422f7661de34a840ab4a27fd40568bd9f6ec
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 21:59:54 +0900

    feat: implement StageFactory

---
+++ Assets/Scripts/Stage/StageFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class StageFactory
+{
+    public Stage BuildStage(GameContext gameContext, StageData stageData)
+    {
+        GameObject stageRoot = new GameObject("StageRoot");
+        if (stageData.floorPrefab != null)
+        {
+            Stage stage = stageRoot.AddComponent<Stage>();
+            stage.initialize(gameContext, stageData);
+            return stage;
+        }
+        return null;
+    }
+
+    public void DestoryStage(GameObject stageRoot)
+    {
+        GameObject.Destroy(stageRoot);
+    }
+}
