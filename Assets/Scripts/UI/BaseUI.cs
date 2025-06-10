ae206f3b63e2dadd1f8d902477eb036b06306672
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-10 21:58:48 +0900

    feat: implement base ui for inherit

---
+++ Assets/Scripts/UI/BaseUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseUI : MonoBehaviour
+{
+    protected UIManager uiManager;
+    protected GameContext gameContext;
+
+    public virtual void Initialize(GameContext gameContext, UIManager uiManager)
+    {
+        this.gameContext = gameContext;
+        this.uiManager = uiManager;
+    }
+
+    protected abstract UIState GetUIState();
+    public void SetActive(UIState state)
+    {
+        gameObject.SetActive(GetUIState() == state);
+    }
+}
