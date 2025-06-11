98998d09d8895ff38248f3d1cbb968527f2c587c
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-11 11:20:19 +0900

    refactor: move initialization code

--- Assets/Scripts/Context/GameContext.cs
+++ Assets/Scripts/Context/GameContext.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 
 public class GameContext
 {
-    public Stage stage;
-    public Player player;
-    public HashSet<Enemy> enemies;
+    public Stage stage = null;
+    public Player player = null;
+    public HashSet<Enemy> enemies = new HashSet<Enemy>();
 
-    public List<StageData> stageDataList;
-    public PlayerData playerData;
+    public List<StageData> stageDataList = null;
+    public PlayerData playerData = null;
 
-    public StageFactory stageFactory;
-    public PlayerFactory playerFactory;
-    public EnemyFactory enemyFactory;
+    public StageFactory stageFactory = new StageFactory();
+    public PlayerFactory playerFactory = new PlayerFactory();
+    public EnemyFactory enemyFactory = new EnemyFactory();
 
     public GameContext(string stageDatasDir, string playerDataPath)
     {
         stageDataList = new List<StageData>( Resources.LoadAll<StageData>(stageDatasDir));
         playerData = Resources.Load<PlayerData>(playerDataPath);
-        enemies = new HashSet<Enemy>();
-        this.stageFactory = new StageFactory();
-        this.playerFactory = new PlayerFactory();
-        this.enemyFactory = new EnemyFactory();
     }
 }
